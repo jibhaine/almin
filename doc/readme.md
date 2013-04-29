d9bee5a11048c05960ca281b997d0eeb3c8aa481
Benjamin Legrand <jibhaine@gmail.com>
2013-04-29 21:57:18 +0200

    switching to node.js, deleting old php files

--- doc/readme.md
+++ doc/readme.md
@@ -1,1 +1,4 @@
 #ALMIN : Application Lifecycle Management Interface
+
+La couche applicative de l'application est gérée par backbone et underscore.
+
--- public/scripts/almin.js
+++ public/scripts/almin.js
@@ -1,27 +1,27 @@
-App = changer.pg.App.extend({
-	postInitialize: function() {
-		//this.hello();
-	},
-	hello: function() {
-		this.alert('HELLO_WORLD');
-	}
+Almin = _.extend({
+    postInitialize: function() {
+        //this.hello();
+    },
+    hello: function() {
+        this.alert('HELLO_WORLD');
+    }
 });
 
 function bodyLoad() {
-	document.addEventListener('backbutton', function(e) {
-		e.preventDefault();
-	}, true);
-	document.addEventListener('touchmove', function(e) {
-	}, false);
-	document.addEventListener('deviceready', deviceReady, false);
-	// NOT IN PHONEGAP MODE
-	if(/https?:\/\//.test(document.location.href)) {
-		deviceReady();
-	}
+    document.addEventListener('backbutton', function(e) {
+        e.preventDefault();
+    }, true);
+    document.addEventListener('touchmove', function(e) {
+    }, false);
+    document.addEventListener('deviceready', deviceReady, false);
+    // NOT IN PHONEGAP MODE
+    if(/https?:\/\//.test(document.location.href)) {
+        deviceReady();
+    }
 }
 
 function deviceReady() {
-	if(!window.app) {
-		window.app = new App();
-	}
+    if(!window.app) {
+        window.app = new Almin();
+    }
 }
